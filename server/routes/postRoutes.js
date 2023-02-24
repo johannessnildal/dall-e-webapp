276dc8c50705e66b74633fef8870ebd0094bd336
Johannes Snildal <johannes.snildal@gmail.com>
2023-02-24 21:20:19 +0100

    bug fixing

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -14,18 +14,20 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// GET ALL POSTS
 router.route("/").get(async (req, res) => {
   try {
     const posts = await Post.find({});
-
     res.status(200).json({ success: true, data: posts });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error });
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Fetching posts failed, please try again",
+      });
   }
 });
 
-// CREATE POST
 router.route("/").post(async (req, res) => {
   try {
     const { name, prompt, photo } = req.body;
@@ -37,9 +39,14 @@ router.route("/").post(async (req, res) => {
       photo: photoUrl.url,
     });
 
-    res.status(201).json({ success: true, data: newPost });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error });
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Unable to create a post, please try again",
+      });
   }
 });
 
